b84bc3ffe4f0e161e603df3ae2456658a1ba169b
mattbluhour <matt@bluhour.agency>
2025-12-17 10:49:40 -0500

    upgraded nextjs, and updated all type errors

--- src/Header/hooks/revalidateHeader.ts
+++ src/Header/hooks/revalidateHeader.ts
@@ -5,13 +5,13 @@ import { revalidateTag } from 'next/cache'
 export const revalidateHeader: GlobalAfterChangeHook = ({ doc, req: { payload, context } }) => {
   if (!context.disableRevalidate) {
     payload.logger.info(`Revalidating header`)
-    
-   if (doc.navItems) {
+
+    if (doc.navItems) {
       doc.navItems.forEach((item: any, i: number) => {
         payload.logger.info(`Nav Item ${i}: ${item.link?.label}`)
       })
     }
-    revalidateTag('global_header')
+    revalidateTag('global_header', 'default')
   }
 
   return doc
